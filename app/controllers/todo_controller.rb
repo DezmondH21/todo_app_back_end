7adde17836d0472e50b076a46717e46e90f5747a
Dezmond Hallman <hallmandezmond@gmail.com>
2017-07-20 20:21:10 +0000

    add rcav for the new and create actions

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -4,4 +4,14 @@ class TodoController < ApplicationController
     def show
         @todo = Todo.find_by_id(params[:id])
     end
+    def new
+    end
+    def create
+        t = todo.new
+        t.description = params['description']
+        t.date = params['date']
+        t.save
+        redirect_to "/todo/show/#{ t.id }"
+    end
 end
+
