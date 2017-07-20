5c5b658dfbf805f482a01860de6a5c74e1fb3298
Dezmond Hallman <hallmandezmond@gmail.com>
2017-07-20 19:23:36 +0000

    extras

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -2,20 +2,6 @@ class TodoController < ApplicationController
     def index
     end 
     def show
-        todo_id = params[:id]
-        
-        
-        
-        if todo_id == '1'
-            @todo_description="do dishes"
-        elsif todo_id == '2'
-            @todo_description="do homework"
-        elsif todo_id == '3'
-            @todo_description="clean room"
-        elsif todo_id == '4'
-            @todo_description="play football"
-        elsif todo_id == '5'
-            @todo_description="play the Xbox One"
-        end
+        @todo = Todo.find_by_id(params[:id])
     end
 end
