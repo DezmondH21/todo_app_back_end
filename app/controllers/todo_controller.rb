0db63ec10937c19ef27e7ea2586363fa61fac609
Dezmond Hallman <hallmandezmond@gmail.com>
2017-07-20 14:08:15 +0000

    todo id based off user input

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -2,6 +2,20 @@ class TodoController < ApplicationController
     def index
     end 
     def show
-        @todo_description="do dishes" 
+        todo_id = params[:id]
+        
+        
+        
+        if todo_id == '1'
+            @todo_description="do dishes"
+        elsif todo_id == '2'
+            @todo_description="do homework"
+        elsif todo_id == '3'
+            @todo_description="clean room"
+        elsif todo_id == '3'
+            @todo_description="play football"
+        elsif todo_id == '4'
+            @todo_description="play the Xbox One"
+        end
     end
 end
