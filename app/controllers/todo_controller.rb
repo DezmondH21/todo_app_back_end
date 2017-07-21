dee561cceb5055ad3514d4e41a85aba340729765
Dezmond Hallman <hallmandezmond@gmail.com>
2017-07-21 12:25:46 +0000

    routes update

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -1,11 +1,14 @@
 class TodoController < ApplicationController
     def index
     end 
+    
     def show
         @todo = Todo.find_by_id(params[:id])
     end
+    
     def new
     end
+    
     def create
         t = todo.new
         t.description = params['description']
@@ -13,5 +16,30 @@ class TodoController < ApplicationController
         t.save
         redirect_to "/todo/show/#{ t.id }"
     end
+    
+    def edit
+        @todo=todo.find_by_id(params[:id])
+    end
+    
+    def update
+         t = todo.new
+         t.description = params['description']
+         t.date = params['date']
+         t.save
+        redirect_to "/todo/show/#{ t.id }"
+    end
+    
+    def create
+         t = todo.new
+         t.description = params['description']
+         t.date = params['date']
+         t.save
+        redirect_to "/todo/show/#{t.id }"
+    end
+    
+    def destroy
+        t = Todo.find_by_id(params[:id])
+        t.destroy
+        redirect_to "/todo/index"
+    end
 end
-
